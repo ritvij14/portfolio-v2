118bd8edd7588a60ac1cc2cd8a869f1c9b8fcba8
Ritvij Kumar Sharma <ritvij2001@gmail.com>
2026-04-03 01:30:15 +0530

    :sparkles: Enable prefetch and add Sharp dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'hover'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
